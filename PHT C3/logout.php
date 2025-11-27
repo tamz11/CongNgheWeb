0b3f8ea07eeed1376d06009058e23642401d0277
tamz11 <mintam212005@gmail.com>
2025-11-27 10:31:09 +0700

    clear

--- PHT C3/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();   // Xóa tất cả biến session
-session_destroy(); // Hủy session
-header('Location: login.html');
-exit;
-?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# CongNgheWeb
-Repo
